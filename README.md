fd67b2563668240734e1ce5bd5d14db3ec6402d9
Arham Islam <arham.m.islam@gmail.com>
2025-07-12 22:36:06 -0400

    Updated files

--- README.md
+++ README.md
@@ -3,13 +3,8 @@ A tool to compare your Instagram followers and following lists to identify non-m
 
 ## Getting Started
 1. **Install Python**
-    - Go to the [official Python website](https://www.python.org/) and download Python 3.13.3
+    - Go to the [official Python website](https://www.python.org/) and download Python 3.13.4
     - During installation, check **Add python.exe to PATH**, then select **Install Now** to get pip (Python's package installer)
-    - After installation, verify that Python and pip are installed by running:
-        ```bash
-        python --version
-        pip --version
-        ```
 
 2. **Download MutualCheck**
     - If you are not already on the repository page, go to the [GitHub Repository Page](https://github.com/arhamislam/mutual-check)
@@ -18,18 +13,25 @@ A tool to compare your Instagram followers and following lists to identify non-m
     - Extract the ZIP file to a folder on your computer
 
 3. **Set Up Your Instagram Credentials**
-    - Open a terminal
+    - Open a terminal (preferably Command Prompt if on Windows)
     - Navigate to the extracted folder:
         - On Windows, run `cd path\to\your\folder`
         - On Linux/MacOS, run `cd path/to/your/folder`
     - Copy `.env.example` to `.env` by running `cp .env.example .env`
     - Open `.env` in a text editor of your choice and fill in your Instagram credentials by replacing the placeholder values, then save the file
 
-4. **Install Dependecies**
+4. **Creating a Virtual Environment**
+    - On your terminal, run `python -m venv .venv`
+    - Activate the virtual environment:
+        - On Windows, run `.venv\Scripts\activate`
+        - On Linux/MacOS, run `source .venv/bin/activate`
+
+5. **Install Dependecies**
     - On your terminal, run `pip install -r requirements.txt`
 
-5. **Run**
+6. **Run**
     - On your terminal, run `python main.py`
+    - Once finished, run `deactivate` to deactivate the virtual environment
 
 ## Notes
 - Make sure Google Chrome is installed
@@ -37,4 +39,4 @@ A tool to compare your Instagram followers and following lists to identify non-m
 - This project is for personal use only. Use responsibly and be aware that automating Instagram may violate their Terms of Service
 
 ## Credits
-Developed by [@arhamislam](https://github.com/arhamislam) on May XX, 2025.
+Developed by [@arhamislam](https://github.com/arhamislam) on June XX, 2025.
--- main.py
+++ main.py
@@ -5,42 +5,72 @@ Description: Loads Instagram credentials from a .env file, automates Instagram, 
 """
 
 import os
+import logging
 from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
-def login(driver, username, password):
-    driver.get("https://www.instagram.com/")
-
-    driver.implicitly_wait(1)
-
-    u = driver.find_element(By.NAME, "username")
-    p = driver.find_element(By.NAME, "password")
-
-    u.send_keys(username)
-    p.send_keys(password)
-
-    l = driver.find_element(By.XPATH, "//button[@type='submit']")
-    l.click()
-
-def main():
+def load_credentials():
+    """Load Instagram credentials from .env file"""
     # Load .env contents and assign to variables
     load_dotenv()
     username = os.getenv("INSTAGRAM_USERNAME")
     password = os.getenv("INSTAGRAM_PASSWORD")
 
+    if not username or not password:
+        raise ValueError("Missing Instagram credentials")
+
+    return username, password
+
+def login(driver, username, password):
+    driver.get("https://www.instagram.com/")
+
+    driver.implicitly_wait(5)
+
+    usernameInput = driver.find_element(By.NAME, "username")
+    passwordInput = driver.find_element(By.NAME, "password")
+
+    usernameInput.send_keys(username)
+    passwordInput.send_keys(password)
+
+    loginButton = driver.find_element(By.XPATH, "//button[@type='submit']")
+    loginButton.click()
+
+def get_profile(driver, username):
+    driver.implicitly_wait(5)
+
+    profile_link = f"//a[starts-with(@href, '/{username}')]"
+    profile = driver.find_element(By.XPATH, profile_link)
+    profile.click()
+
+    print("here")
+
+def get_followers_list(driver):
+    driver.implicitly_wait(5)
+
+    
+
+def main():
     # Create driver
-    driver = webdriver.Chrome()
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option("detach", True)
+    options.add_experimental_option("excludeSwitches", ["enable-logging"])
+    os.environ["TF_CPP_MIN_LOG_LEVEL"] = '3'
+    logging.getLogger("tensorflow").setLevel(logging.ERROR)
+
+    driver = webdriver.Chrome(options = options)
 
     # Automation
     try:
+        username, password = load_credentials()
         login(driver, username, password)
+        get_profile(driver, username)
+        get_followers_list(driver)
     
     # Quit
     finally:
-        driver.quit()
+        # driver.quit()
+        print("END")
 
 if __name__ == "__main__":
     main()
